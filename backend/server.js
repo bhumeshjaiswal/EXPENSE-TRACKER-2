ce08c559002ed24354976611a350e2e1782f578c
bhumeshjaiswal <bhumeshjaiswal.sae.it@gmail.com>
2026-01-29 21:53:01 +0530

    Implement DELETE endpoint for expenses
    Added DELETE endpoint to remove expenses by ID.

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Expense = require("./models/expense"); // because your file is expense.js
+const Expense = require("./models/expense");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/expenseDB")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-// Add expense
+// ADD expense
 app.post("/add-expense", async (req, res) => {
   const expense = new Expense(req.body);
   await expense.save();
   res.json({ message: "Expense Added" });
 });
 
-// Get all expenses
+// GET all expenses
 app.get("/expenses", async (req, res) => {
   const data = await Expense.find();
   res.json(data);
+});
+
+// DELETE expense
+app.delete("/delete-expense/:id", async (req, res) => {
+  await Expense.findByIdAndDelete(req.params.id);
+  res.json({ message: "Expense Deleted" });
 });
 
 // Start server
